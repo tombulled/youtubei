23e9bedebd3e89749f42ec7f6644916139e58191
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2024-01-21 17:11:52 +0000

    Move to v2 api, with client-specific sub-packages - implement WEB.parse_guide

--- app.v2.py
+++ app.v2.py
@@ -11,7 +11,19 @@ from rich.pretty import pprint as pp
 
 WEB = InnerTube("WEB", "2.20240105.01.00")
 
-guide_raw = WEB.adaptor.dispatch("guide")
-data = youtubei.WEB.parse_guide(guide_raw)
+class WEB:
+    client = InnerTube("WEB", "2.20240105.01.00")
+    parser = youtubei.WEB
 
-pp(data)
+    @classmethod
+    def guide(cls):
+        response = cls.client.adaptor.dispatch("guide")
+
+        return cls.parser.parse_guide(response)
+
+# guide_raw = WEB.adaptor.dispatch("guide")
+# data = youtubei.WEB.parse_guide(guide_raw)
+
+d = WEB.guide()
+
+pp(d)
