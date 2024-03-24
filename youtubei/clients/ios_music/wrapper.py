ff9446cf01c121a010e843dd0b1498df766e7bca
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2024-03-24 15:50:42 +0000

    Fix mypy static typing issues

--- youtubei/clients/ios_music/wrapper.py
+++ youtubei/clients/ios_music/wrapper.py
@@ -15,7 +15,7 @@ class IosMusic:
     client: InnerTube = field(default=IOS_MUSIC_CLIENT)
 
     def __repr__(self) -> str:
-        client_version: str = self.client.adaptor.context.client_version
+        client_version: str = self.client.adaptor.context.client_version  # type: ignore
 
         return f"{type(self).__name__}({client_version!r})"
 
--- youtubei/models/colour.py
+++ youtubei/models/colour.py
@@ -16,6 +16,5 @@ class Colour(BaseModel):
 
 class ColourPalette(BaseModel):
     section2_color: Optional[int] = None
-    icon_inactive_color: Optional[int] = None
     icon_disabled_color: Optional[int] = None
     icon_inactive_color: Optional[int] = None
